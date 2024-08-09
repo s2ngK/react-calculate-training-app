d61f11243c8dc0091ca420c2f8f2bbac13294a44
ssng.kang <ssng.kang@gmail.com>
2024-08-09 13:20:08 +0900

    setting prettier

---
+++ src/containers/KeyPadContainer.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const KeyPadContainer = () => {
+  const number2 = useState(1);
+  return <>hello im KeyPadContainer {number2}</>;
+};
+
+export default KeyPadContainer;
