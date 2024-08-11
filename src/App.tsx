da922e07f50d730eece1eb10b83c678714eb8e52
ssng.kang <ssng.kang@gmail.com>
2024-08-12 03:09:33 +0900

    install sass
    install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
+import './App.scss';
 import KeyPadContainer from './containers/KeyPadContainer';
 
 function App() {
   return (
     <div className='App'>
       hello world
+      <p className='scss-test'>align right</p>
       <KeyPadContainer />
     </div>
   );
